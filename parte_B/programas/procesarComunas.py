f87939754a1611e138d779bf4a71b691edbe2c75
CarolinaObreque <carolina.obrequehigueras@gmail.com>
2023-06-21 19:57:43 -0400

    formato

--- parte_B/programas/procesarComunas.py
+++ parte_B/programas/procesarComunas.py
@@ -1,14 +1,21 @@
 #output = open("infoComunasOutputArray","w")
 import os
 import codecs
+import unicodedata
+def remove_accents(text):
+    return ''.join(c for c in unicodedata.normalize('NFD', text) if unicodedata.category(c) != 'Mn')
 
 outputFile = codecs.open("../datos/comunasProcesadas.txt","w","utf-8")
-with codecs.open("infoPais.txt","r","utf-8") as file:
+with codecs.open("../datos/infoPais.txt","r","utf-8") as file:
 #    output.write("[")
     lines = file.readlines()
     outputLines = []
     for line in lines:
-        outputLines.append(line.strip().split("	"))
+        line = line.strip()
+        line = remove_accents(line)
+        line = line.lower()
+        line = line.capitalize()
+        outputLines.append(line.split("	"))
     outputFile.write(str(outputLines))
 
 
