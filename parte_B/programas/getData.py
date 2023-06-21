9d22552527211f6a76c3855d794cdae33ad061b0
CarolinaObreque <carolina.obrequehigueras@gmail.com>
2023-06-21 17:14:46 -0400

    regiones

--- parte_B/programas/getData.py
+++ parte_B/programas/getData.py
@@ -2,10 +2,14 @@ import csv
 
 # Datos que deseas escribir en el archivo CSV
 datosPais = [
-    ["Chile","1"],
+    ["Chile"],
     
 ]
-datosRegion = []
+datosRegion = [["Region de Tarapaca",1],["Region de Antofagasta",1],["Region de Atacama",1],["Region de Coquimbo",1],
+["Region de Valparaiso",1],["Region del Libertador General Bernando O'Higgins",1],["Region del Maule",1],
+["Region del Bio-bio",1],["Region de la Araucania",1],["Region de Los Lagos",1],["Region Aysen del General Carlos Ibanez del Campo",1],
+["Region de Magallanes y Antartica Chilena",1],["Region Metropolitana de Santiago",1],["Region de Los Rios",1],
+["Region de Arica y Parinacota",1],["Region de Nuble",1]]
 datosComuna = []
 datosTener = []
 datosBienestar = []
@@ -23,5 +27,9 @@ Bienestar = 'INFO133-equipo09\parte_B\datos\Bienestar.csv'
 with open(Pais, 'w', newline='') as csv_file:
     writer = csv.writer(csv_file)
     writer.writerows(datosPais)
+
+with open(Region, 'w', newline='') as csv_file:
+    writer = csv.writer(csv_file)
+    writer.writerows(datosRegion)
 
 print("Archivo CSV creado exitosamente.")
