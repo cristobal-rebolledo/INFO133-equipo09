b57078798dbe859e0ca64a5371696587ef6c4ac3
Benjamín <benrrixworks@gmail.com>
2023-06-21 22:23:13 -0400

    Modificado quita.py para que saque los tildes de DMCStasa.csv

--- quita.py
+++ quita.py
@@ -4,9 +4,9 @@ import codecs
 def remove_accents(text):
     return ''.join(c for c in unicodedata.normalize('NFD', text) if unicodedata.category(c) != 'Mn')
 
-file = codecs.open("Salud.csv","r","utf-8")
+file = codecs.open("DMCStasa.csv","r","utf-8-sig")
 text = "".join(file.readlines())
-output = open("Salud_salida.csv","w")
+output = codecs.open("DMCS_Tasa_Salida.csv","w","utf-8-sig")
 output.write(remove_accents(text))
 file.close()
 output.close()
