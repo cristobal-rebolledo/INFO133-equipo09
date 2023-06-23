8f5b58644326480dcc218d136ae76ea69cfe8ee2
Benjamín <benrrixworks@gmail.com>
2023-06-22 20:31:47 -0400

    quita.py ahora quita las comillas

--- quita.py
+++ quita.py
@@ -2,11 +2,11 @@ import unicodedata
 import codecs
 
 def remove_accents(text):
-    return ''.join(c for c in unicodedata.normalize('NFD', text) if unicodedata.category(c) != 'Mn')
+    return ''.join(c for c in unicodedata.normalize('NFD', text) if unicodedata.category(c) != 'Mn').replace('"',"").replace("'","")
 
-file = codecs.open("DMCStasa.csv","r","utf-8-sig")
+file = codecs.open("estadios_Salida.csv","r","utf-8-sig")
 text = "".join(file.readlines())
-output = codecs.open("DMCS_Tasa_Salida.csv","w","utf-8-sig")
+output = codecs.open("estadios_Salida_SIN_TILDES.csv","w","utf-8-sig")
 output.write(remove_accents(text))
 file.close()
 output.close()
